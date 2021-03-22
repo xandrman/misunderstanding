292a5edd1d32afdf930eaf1dfd337fa3dc8faf3b
xandrman <xandrman@yandex.ru>
2021-03-22 13:44:11 +0600

    Add Event Class

---
+++ app/src/main/java/pro/xandr/misunderstanding/Event.java
@@ -0,0 +1,45 @@
+package pro.xandr.misunderstanding;
+
+import java.util.Date;
+import java.util.UUID;
+
+public class Event {
+
+    private UUID uuid;
+    private String title;
+    private Date date;
+    private boolean solved;
+
+    public Event() {
+        uuid = UUID.randomUUID();
+        date = new Date();
+    }
+
+    public UUID getUuid() {
+        return uuid;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public boolean isSolved() {
+        return solved;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public void setSolved(boolean solved) {
+        this.solved = solved;
+    }
+}
