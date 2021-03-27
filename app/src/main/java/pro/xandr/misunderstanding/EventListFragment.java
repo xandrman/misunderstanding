348aa9da05c49e4f7d7a1df8a6e0bfd7feb8f642
xandrman <xandrman@yandex.ru>
2021-03-27 20:36:00 +0600

    Added image for solved events

--- app/src/main/java/pro/xandr/misunderstanding/EventListFragment.java
+++ app/src/main/java/pro/xandr/misunderstanding/EventListFragment.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -34,12 +35,14 @@ public class EventListFragment extends Fragment {
 
         private TextView tvTitle;
         private TextView tvDate;
+        private ImageView ivSolved;
         private Event event;
 
         public EventHolder(LayoutInflater inflater, ViewGroup parent) {
             super(inflater.inflate(R.layout.event_list_item, parent, false));
             tvTitle = itemView.findViewById(R.id.tv_title);
             tvDate = itemView.findViewById(R.id.tv_date);
+            ivSolved = itemView.findViewById(R.id.iv_solved);
             itemView.setOnClickListener(this);
         }
 
@@ -47,6 +50,7 @@ public class EventListFragment extends Fragment {
             this.event = event;
             tvTitle.setText(event.getTitle());
             tvDate.setText(event.getDate().toString());
+            ivSolved.setVisibility(event.isSolved() ? View.VISIBLE : View.GONE);
         }
 
         @Override
